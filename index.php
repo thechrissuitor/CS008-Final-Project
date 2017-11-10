1d6eea42e7fdbd04983f2b91e01c80de0a024ce1
nnimako <33186557+nnimako@users.noreply.github.com>
2017-11-09 20:28:25 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php
+include ('top.php');
+?>
+
+<div class="flexslider">
+    <ul class="slides">
+        <li>
+            <img src="images/happy1.jpg" alt="" >
+        </li>
+        <li>
+            <img src="images/happy5.jpg" alt="" >
+        </li>
+
+        <li>
+            <img src="images/happy2.jpg" alt="" >
+        </li>
+
+        <li>
+            <img src="images/sad4.jpg" alt="" >
+        </li>
+        
+         <li>
+             <img src="images/happy6.jpeg" alt="" >
+        </li>
+        
+         <li>
+            <img src="images/sad5.jpg" alt="" >
+        </li>
+
+    </ul>
+</div>
+
+<article>
+    <figure>
+        <img  class="" alt="" src="images/sad9.jpg">
+        <figcaption> </figcaption>
+    </figure>
+    
+    <p>
+        Dogs today are beaten, chained, and starved. They face a lot of horrendous amounts of
+        trauma, and families refuse to take them because they are left mentally and physically scarred.
+        The purpose of Save Our Sweeties(SOS) is to inspire families and individuals looking
+        for dogs to rescue them. Take in the sad, broken dog and your reward will be much greater than
+        simply adopting a clean puppy from the nearest pet store. Your love, care, and affection
+        can save lives. To a recovering rescued dog, your love will be the most important thing in their
+        life, and in return, the dog shall love you unconditionally.
+    </p>
+</article>
+
+<?php
+include ('footer.php');
+?>
