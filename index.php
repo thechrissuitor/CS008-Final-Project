6db87f9033d0fda11cd8a93ac74d96d9a451ca80
thechrissuitor <33239632+thechrissuitor@users.noreply.github.com>
2017-11-12 03:46:21 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -31,19 +31,24 @@ include ('top.php');
 </div>
 
 <article>
-    <figure>
-        <img  class="" alt="" src="images/sad9.jpg">
+    <figure class="gunner">
+        <img  class="gunner_circle" alt="" src="images/sad9.jpg">
         <figcaption> Gunner </figcaption>
     </figure>
     
     <p>
-        Dogs today are beaten, chained, and starved. They face a lot of horrendous amounts of
-        trauma, and families refuse to take them because they are left mentally and physically scarred.
-        The purpose of Save Our Sweeties(SOS) is to inspire families and individuals looking
-        for dogs to rescue them. Take in the sad, broken dog and your reward will be much greater than
-        simply adopting a clean puppy from the nearest pet store. Your love, care, and affection
-        can save lives. To a recovering rescued dog, your love will be the most important thing in their
-        life, and in return, the dog shall love you unconditionally.
+        Dogs today are beaten, chained, and starved. The amount of abuse, between neglectful and abusive owners all the way up through dog fighting rings, is unimaginably immense and wrong. They go through horrendous amounts of trauma and cruelty, and families refuse to take them because they are left physically and mentally scarred, even though a family is the key to getting them better. The purpose of Save Our Sweeties is the inspire families looking for dogs to adopt to rescue them. If you’re already looking for a dog, then all you need to do is take in a single dog to change the course of its life. As a result, if you take in a sad, broken dog, then your reward will be much greater than simply adopting a clean puppy from the nearest pet store. These dogs have had everything taken from them, and your love, care, and affection can save their lives. To a recovering rescued dog, you love will be the most important thing in their life, and, in return, the dog shall love you unconditionally. Please browse our website to get an idea of what these dogs go through.<br>
+            <br/>
+See our abuse page in order to get an idea about specifically what trauma these dogs endure. The idea here isn’t to make you sad, but more to give you the strength to adopt a rescue dog instead of a “clean” dog, and the evidence to back up your decision and reaffirm that you’re doing the right thing.<br/><br/>
+See our new home tab to find out what it’s been like for current and past families that have adopted a rescue dog. Hear heartwarming testimonials about how the dog grew because of the family, and how the family grew because of the dog.<br/><br/>
+Check out the rehab page in order to see the process of rehabilitation that the dogs go through. We’ll bring you as close to the “Big Dogs Huge Paws” non-profit organization as we can, so you can see all the amazing things that they do, from training their staff to emotionally and physically healing the dogs to keeping their buildings and stores stocked with high quality food and supplies in order to give both themselves and nearby families the access to proper equipment they need to give these dogs the life they have always deserved.<br/><br/>
+    <figure class="duke">
+        <img  class="gunner_circle" alt="" src="images/duke.jpeg">
+        <figcaption> Duke </figcaption>
+    </figure>
+Check out our photos tab to see a plethora of photos. Here you can actually experience the heartbreak of these dogs after their abuse, the embodiment of perseverance and persistence of their recovery, and the unequalled joy of their new home/family. More than that, you can experience this across breeds of dogs, letting you connect with the breed that you love the most.<br/><br/>
+The form page is where you can let us know what we did well, and what we need to improve upon in our website. It also lets us get a chance to know about your relationship with dogs.<br/><br/>
+The about us is just where you can find out about the web developers.
     </p>
 </article>
 
