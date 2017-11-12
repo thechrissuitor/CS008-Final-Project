40cb42caaf99a06539e6915774c89d7bd372e7dd
nnimako <33186557+nnimako@users.noreply.github.com>
2017-11-12 02:21:44 -0500

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,5 +1,25 @@
 <!-- ######################    Start Footer  #################################### -->
 <footer>
-	<p>By Christopher Suitor, Nana Nimako, Kieran Edraney</p>
+  
+    <ul>
+
+        <li> <a href="index.php">Home</a></li>
+        <li> <a href="photos.php">Photos</a></li>
+        <li> <a href="drhc.php">Dog Rehab & Home Coming</a></li>
+        <li> <a href="form.php">Sign Up </a></li>
+        <li> <a href="abuse.php">Abuse</a></li>
+        <li> <a href="aboutus.php">About Us</a></li>
+
+
+    </ul>
+    
+    <address>
+        University of Vermont:<br>
+         Burlington, VT 05405<br>
+          (802)656-3131<br>
+    </address>
+    
+    <p>Find Us:</p><br>
+    
 </footer>
 <!-- ######################    End Footer  #################################### -->
