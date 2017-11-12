0c8f58eaaa85c3f6d2ee6ae54efc51995dba57e4
nnimako <33186557+nnimako@users.noreply.github.com>
2017-11-12 00:39:53 -0500

    Add files via upload

--- header.php
+++ header.php
@@ -1,7 +1,19 @@
 <!-- ######################     Start Page header   ############################## -->
 
 <header>
-    <h1>Ruff Rescue</h1>
+    <div class="dropdown">
+        <img class="menuicon" alt="" src="images/menu.png">
+        
+        <div class="dropdown-content">
+            <a href="index.php">Home</a>
+            <a href="photos.php">Photos</a>
+            <a href="drhc.php">Rehab & Home Coming</a>
+            <a href="form.php">Form</a>
+            <a href="abuse.php">Abuse</a>
+            <a href="aboutus.php">About Us</a>
+        </div>
+    </div>
+    <h1>Save Our Sweeties</h1>
 </header>
 
 <!-- ######################     End Page header   ############################## -->
