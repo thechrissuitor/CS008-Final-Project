cbbd05302adf30ea0f627c59e068c496c2db2a19
nnimako <33186557+nnimako@users.noreply.github.com>
2017-11-15 11:55:01 -0500

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,76 @@
+<?php
+include ('top.php');
+?>
+<h2 class="thank">Thank You!</h2>
+<article>
+    <figure>
+        <img class="nnk"
+             alt="" src="images/nnk.jpg">
+    </figure>
+    <p>
+        A huge thanks to "Big Dogs Huge Paws" in Houston Texas (their headquarters is 
+        in Colorado, with many locations throughout the US) for letting us use 
+        their photos, their testimonies and everything that they have provided us.
+        We’d also like to thank this amazing organization for all the love and care
+        that they provide these dogs in need. These dogs can come from the worst 
+        of situations, and the work that “Big Dogs Huge Paws” does in order to not
+        only train and educate their talented staff, but to provide access across
+        the United States to high quality dog food, leashes, cleaning supplies, 
+        grooming supplies, and more. The work that is done at “Big Dogs Huge Paws” 
+        inspired us to make this website and will continue to inspire us as we go
+        on in life. So once again, thank you for all the work you do, and we wish
+        you the best, “Big Dogs Huge Paws.”  
+    </p>
+    <br><br>
+    <p>
+    <figure>
+        <img class="chris" alt="" src="images/happy3.jpg">
+    </figure>
+    Thank you, Professor Robert Erickson, for teaching us the art of website 
+    design. Time has moved so quickly in this class (even when we were 
+    working on forms for a month) but we still managed to learn a lot 
+    about HTML, PHP and CSS. Hopefully at least some of us will see you 
+    soon in another one of your website design classes in the not so 
+    distant future!
+
+    Thank you, Professor Joan Rosebush, for helping us come up with a name for our 
+    website. This was a big undertaking for us, and every little bit helps, even 
+    the seemingly innocuous but extremely important task of choosing a name for 
+    our website. We’re all excited to see you next semester in Calculus II, assuming
+    we can get into your class!
+
+</p>
+
+<p>
+    Thank you, TAs, for putting in the hours, giving us advice, answering 
+
+    our questions, and all with a smile and a great attitude. We also want to
+    thank you for being able to put up with Bob!
+
+    We also want to thank the dogs for being so amazing by adapting to their 
+    situation and taking in the love that “Big Dogs Huge Paws” has been giving
+    them. The events you’ve endured are harrowing, and to think that you all 
+    came out on top of it is inspiring to us all.<br><br>
+<figure>
+    <img class="jon" alt="" src="images/jon.jpg">
+</figure>
+    Christopher T. Suitor: “A huge thank you goes to Ms. Jeanne. My love for 
+    dogs bloomed while spending time volunteering with you.  Also, a gigantic
+    thank you goes to Eve. I love you, Eve! Thank you for being so adorable,
+    sweet, and everything good!”<br><br>
+
+    Kieran M. Edraney: “Thanks to my mom and sister! You guys are both supper
+    supportive of me and have helped me a ton both in college and getting here.
+
+    Mikie, you’re pretty cool too and I’m excited to get to know you. See you soon!”
+    <br><br>
+    Nana Nimako: "To all friends and families out there who support me to go higher
+    and support my dreams. I would like to take this great platform to thank you.
+    I would like to make it clear that also; hold on, for the great times awaits.
+
+</p>
+</article>
+
+<?php
+include ('footer.php');
+?>
