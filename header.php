fda6ccb744aa79caa7cff923f4f0d3f7e92b4926
nnimako <33186557+nnimako@users.noreply.github.com>
2017-11-14 21:49:47 -0500

    Add files via upload

--- header.php
+++ header.php
@@ -4,16 +4,20 @@
     <div class="dropdown">
         <img class="menuicon" alt="" src="images/menu.png">
         
-        <div class="dropdown-content">
+        
+        <nav class="dropdown-content">
             <a href="index.php">Home</a>
             <a href="photos.php">Photos</a>
-            <a href="drhc.php">Rehab & Home Coming</a>
+            <a href="rescue.php">Rescue</a>
+            <a href="rehab.php">Rehabilitate</a>
+            <a href="relish.php">Relish</a>
             <a href="form.php">Form</a>
             <a href="abuse.php">Abuse</a>
             <a href="aboutus.php">About Us</a>
-        </div>
+        </nav>
     </div>
-    <h1>Save Our Sweeties</h1>
+     <img class="logo"alt="" src="images/final_logo.png">
+      <h1>Save Our Sweeties</h1>
 </header>
 
 <!-- ######################     End Page header   ############################## -->
